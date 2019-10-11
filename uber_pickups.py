bec363f086ebed4182332eceeb0dcf8f305aa47b
Naomi <pnaomi@gmail.com>
2019-10-11 13:01:20 -0700

    WIP uber pickups tutorial

--- uber_pickups.py
+++ uber_pickups.py
@@ -25,5 +25,20 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-st.write('Done! (using st.cache)')
+#st.write('Done! (using st.cache)')
+
+st.subheader('Raw data')
+st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+st.bar_chart(hist_values)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)       # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups {hour_to_filter}:00')
+st.map(filtered_data)
+
+
 
